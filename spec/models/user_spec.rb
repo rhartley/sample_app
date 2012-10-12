53a352fcaf9df0262da3164bfc56cb607f8fbcd5
rhartley <rhartley@github.com>
2012-10-12 08:59:37 -0500

    Left it on a branch for some reason

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,6 +40,8 @@ describe User do
     it { should_not be_valid }
   end
 
+
+
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
