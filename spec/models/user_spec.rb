f55f1a8e9875c98756e67baa14ef1b812f2845d3
rhartley <rhartley@github.com>
2012-10-13 09:32:27 -0500

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,7 +22,9 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+
   it { should be_valid }
 
   describe "when name is not present" do
@@ -39,8 +41,6 @@ describe User do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
-
-
 
   describe "when email format is invalid" do
     it "should be invalid" do
@@ -106,6 +106,11 @@ describe User do
 
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
+    end
+
+    describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
     end
   end
     
